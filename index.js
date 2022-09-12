4e4fb9102874f7ed6c1eeada8410ef5977abb1cf
AmirHosein-Vahed <vahed.ahv@gmail.com>
2022-09-12 22:20:33 +0430

    post created

--- index.js
+++ index.js
@@ -1,28 +1,40 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
+// add json middleware to app
+app.use(express.json());
 
 const courses = [
-    { id: 1, name: 'Math' },
-    { id: 2, name: 'Physics' },
-    { id: 3, name: 'Robotics' },
-    { id: 4, name: 'Geometry' },
-]
+  { id: 1, name: "Math" },
+  { id: 2, name: "Physics" },
+  { id: 3, name: "Robotics" },
+  { id: 4, name: "Geometry" },
+];
 
-app.get('/', (req, res) => {
-    res.send("Hello world")
-})
+app.get("/", (req, res) => {
+  res.send("Hello world!");
+});
+// get all courses
+app.get("/api/courses", (req, res) => {
+  res.send(courses);
+});
+// get one course detail
+app.get("/api/courses/:id", (req, res) => {
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course) {
+    res.status(404).send("course not found");
+  }
+  res.send(course);
+});
+// create new course
+app.post("/api/courses", (req, res) => {
+  const course = {
+    id: courses.length + 1,
+    name: req.body.name,
+  };
+  courses.push(course);
+  res.send(course);
+});
 
-app.get('/api/courses', (req, res) => {
-    const course = courses.find(c => c.id === parseInt(req.params.id))
-    if (!course) { res.status(404).send("course not found") }
-    res.send(course)
-})
-
-app.get('/api/courses/:id', (req, res) => {
-    // res.send(req.params.id)
-    res.send(courses[req.params.id-1])
-})
-
-const port = process.env.PORT || 3000
-app.listen(port, () => console.log(`Listening on port ${port}...`))
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
